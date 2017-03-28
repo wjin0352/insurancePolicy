94bc6d6c52241ecfe870fd4f54edfca407fee3f0
won <wjin0352@gmail.com>
2017-03-27 23:48:01 -0400

    fix for nested reduxform values on selectboxes!

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -1,74 +1,95 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
+import { Field, FieldArray, reduxForm } from 'redux-form';
 import { createPolicy } from '../modules/createPolicy/policyAction';
 
-const renderField = ({ input, label, type, placeholder, meta: { touched, error, warning } }) => (
+const renderFieldInput = ({ input, label, meta: { touched, error, warning } }) => (
   <div className="form-group">
     <label>{label}</label>
     <div>
-      <input {...input} type={type} placeholder={placeholder}/>
+      <input {...input} />
     </div>
     { touched && (error && <div className="text-help">{error}</div>)}
   </div>
 );
 
+const renderCheckBoxes = ({ input, meta: { touched, error, warning } }) => (
+  <div className="check_boxes">
+    <label>Health Conditions:</label>
+    <div>
+      <label>allergies: </label>
+      <Field name="allergies" component="input" type="checkbox" />
+    </div>
+    <div>
+      <label>sleep apnea: </label>
+      <Field name="sleepApnea" component="input" type="checkbox" />
+    </div>
+    <div>
+      <label>heart disease: </label>
+      <Field name="heartDisease" component="input" type="checkbox" />
+    </div>
+    <div>
+      <label>high cholesterol: </label>
+      <Field name="highChol" component="input" type="checkbox" />
+    </div>
+    <div>
+      <label>asthma: </label>
+      <Field name="asthma" component="input" type="checkbox" />
+    </div>
+    <div>
+      <label>none: </label>
+      <Field name="none" component="input" type="checkbox" />
+    </div>
+  </div>
+);
+
+const conditions = ['allergies', 'sleep apnea', 'heart disease', 'high cholesterol', 'asthma', 'none'];
+
+
 class UserForm extends Component {
   constructor(props) {
-    super(props)
-
-    this.handleData = this.handleData.bind(this);
+    super(props);
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
-  handleData(e) {
-    console.log(e)
-    // this.props.createPolicy
+  
+  handleOnChange = (data) => {
+    let checkedArray = [];
+    console.log('data=====', data);
+  }
+
+  handleFormSubmit = (data) => {
+    this.props.createPolicy(data);
   }
 
   render () {
     const { handleSubmit } = this.props;
     return (
-      <form onSubmit={handleSubmit(this.handleData(e))}>
-        <Field name="firstName" label="First Name" component={renderField} type="text" placeholder="first name" />
-        <Field name="age" label="Age" component={renderField} type="text" placeholder="age"/>
-        <Field name="location" label="location" component={renderField} type="text" placeholder="location"/>
-        <Field name="healthCondition" label="Health Condition" component={renderField} type="text" placeholder="health condition"/>
+      <form onSubmit={handleSubmit(this.props.createPolicy)}>
+        <Field name="firstName" label="First Name" component={renderFieldInput} type="text" placeholder="first name" />
+        <Field name="age" label="Age" component={renderFieldInput} type="text" placeholder="age"/>
+        <Field name="location" label="location" component={renderFieldInput} type="text" placeholder="location"/>
+        <Field name="healthCondition" label="Health Condition" component={renderFieldInput} type="text" placeholder="health condition"/>
         <div>
           <label>Gender</label>
           <div>
             <Field name="gender" component="select">
-              <option></option>
+              <option>select one</option>
               <option value="male">male</option>
               <option value="female">female</option>
             </Field>
           </div>
         </div>
-        <div>
-          <label>Health Conditions:</label>
-          <div>
-            <label>allergies: </label>
-            <Field name="allergies" component="input" label="allergies" type="checkbox"/>
-          </div>
-          <div>
-            <label>sleep apnea: </label>
-            <Field name="sleepApnea" component="input" type="checkbox"/>
-          </div>
-          <div>
-            <label>heart disease: </label>
-            <Field name="heartDisease" component="input" type="checkbox"/>
-          </div>
-          <div>
-            <label>high cholesterol: </label>
-            <Field name="highChol" component="input" type="checkbox"/>
-          </div>
-          <div>
-            <label>asthma: </label>
-            <Field name="asthma" component="input" type="checkbox"/>
-          </div>
-          <div>
-            <label>none: </label>
-            <Field name="none" component="input" type="checkbox"/>
-          </div>
-        </div>
+          { 
+            conditions.map((name, index) => {
+              return (
+                <div>
+                  <label>{name}: </label>
+                  <Field name={`selectBoxes.${name}`} onChange={this.handleOnChange} label={name} component="input" type="checkbox" id={name}/>
+                </div>
+              )
+            })
+          }
         <button type="submit">Submit</button>
       </form>
     );
@@ -93,3 +114,28 @@ export default connect(null, { createPolicy })(reduxForm({
     </div>
   )
 }}/>*/}
+
+  /*<div>
+            <label>allergies: </label>
+            <Field name="allergies" component="input" label="allergies" type="checkbox"/>
+          </div>
+          <div>
+            <label>sleep apnea: </label>
+            <Field name="sleepApnea" component="input" type="checkbox"/>
+          </div>
+          <div>
+            <label>heart disease: </label>
+            <Field name="heartDisease" component="input" type="checkbox"/>
+          </div>
+          <div>
+            <label>high cholesterol: </label>
+            <Field name="highChol" component="input" type="checkbox"/>
+          </div>
+          <div>
+            <label>asthma: </label>
+            <Field name="asthma" component="input" type="checkbox"/>
+          </div>
+          <div>
+            <label>none: </label>
+            <Field name="none" component="input" type="checkbox"/>
+          </div>*/
